c9fdf1d1045e078328e783a8211e70fec0d68c07
ayeolakenny <ayeolakenny@gmail.com>
2022-08-25 12:13:41 +0100

    corrections

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,37 +63,9 @@ export const Contact = () => {
                 </div>
               </div>
             </div>
-            <form name="contact" method="POST" data-netlify="true">
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Role:{" "}
-                  <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select>
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message: <textarea name="message"></textarea>
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
             <div className="contact-right">
               <form method="POST" className="signin-form" netlify>
+                <input type="hidden" name="contact" value="contact" />
                 <div className="input-grids">
                   <input
                     type="text"
